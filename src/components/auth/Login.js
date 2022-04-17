71cc5f609e0f99b4ee53ae704d033c8afb6af547
han vu <tieungpk1@gmail.com>
2022-04-17 18:32:52 +0700

    add return

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Input, Checkbox, Form, message as messageAntd } from 'antd';
-import { Navigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Input, Checkbox, Form, message as messageAntd } from "antd";
+import { Navigate } from "react-router-dom";
 
 export default function Login(props) {
-  const [state, setState] = useState({ username: '', password: '' });
+  const [state, setState] = useState({ username: "", password: "" });
   const { username, password } = state;
   const {
     actions: { login },
@@ -16,22 +16,23 @@ export default function Login(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const onChange = e => {
+  const onChange = (e) => {
     setState({ ...state, [e.target.name]: e.target.value });
   };
 
   const notification = () => {
     if (success) {
       messageAntd.success(message);
-    } else if (message !== '') {
-      messageAntd.error(message || '');
+    } else if (message !== "") {
+      messageAntd.error(message || "");
     }
   };
 
   const onSubmit = () => {
-    if (!username || !password || username === '' || password === '') {
-      messageAntd.error('Bạn chưa nhập tài khoản mật khẩu');
+    if (!username || !password || username === "" || password === "") {
+      return messageAntd.error("Bạn chưa nhập tài khoản mật khẩu");
     }
+
     login({ username, password });
   };
 
@@ -40,19 +41,39 @@ export default function Login(props) {
   return (
     <div>
       <Form name="basic" initialValues={{ remember: false }} autoComplete="off">
-        <Form.Item name="username" rules={[{ required: true, message: 'Vui lòng nhập tên tài khoản!' }]}>
-          <Input placeholder="Tài khoản" name="username" value={username} onChange={onChange} />
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: "Vui lòng nhập tên tài khoản!" }]}
+        >
+          <Input
+            placeholder="Tài khoản"
+            name="username"
+            value={username}
+            onChange={onChange}
+          />
         </Form.Item>
 
-        <Form.Item name="password" rules={[{ required: true, message: 'Vui lòng nhập mật khẩu!' }]}>
-          <Input.Password placeholder="Mật khẩu" name="password" value={password} onChange={onChange} />
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Vui lòng nhập mật khẩu!" }]}
+        >
+          <Input.Password
+            placeholder="Mật khẩu"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
         </Form.Item>
 
         <Form.Item name="remember" valuePropName="checked">
           <Checkbox className="d-flex mt-8">Nhớ mật khẩu</Checkbox>
         </Form.Item>
 
-        <div className="btn-switch on" style={{ margin: '16px 12px 0' }} onClick={() => onSubmit()}>
+        <div
+          className="btn-switch on"
+          style={{ margin: "16px 12px 0" }}
+          onClick={() => onSubmit()}
+        >
           Đăng nhập
         </div>
       </Form>
