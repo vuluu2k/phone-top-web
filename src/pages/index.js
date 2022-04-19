5edeb5f40d2e77231a898e9ca1c7743d497c2c36
vuluu <tieungpk1@gmail.com>
2022-04-20 01:22:09 +0700

    update display

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { default as Account } from './Account';
 export { default as ProductDetail } from './ProductDetail';
 export { default as Cart } from './Cart';
 export { default as Pay } from './Pay';
+export { default as CheckPackage } from './CheckPackage';
